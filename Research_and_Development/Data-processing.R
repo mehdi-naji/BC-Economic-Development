0bf3bcb3a311a3e761dcaa4ec3cfd02e5dbe1c39
Mehdi Naji <snaji001@ucr.edu>
2023-12-29 09:49:01 -0800

    update dfgrowth

--- Research_and_Development/Data-processing.R
+++ Research_and_Development/Data-processing.R
@@ -35,9 +35,10 @@ df_growth <- df1 |>
   arrange(GEO, Funder, Performer, `Science type`, Prices, Year) |>
   filter(Year %in% c(maxyear-5, maxyear-3, maxyear)) |>
   group_by(GEO, Funder, Performer, `Science type`, Prices) |>
-  mutate(GR5 = ifelse(all(c(maxyear-5, maxyear) %in% Year), (VALUE[Year == maxyear] / VALUE[Year == maxyear-5]) - 1, NA),
+  mutate(Maxyear = maxyear,
+         GR5 = ifelse(all(c(maxyear-5, maxyear) %in% Year), (VALUE[Year == maxyear] / VALUE[Year == maxyear-5]) - 1, NA),
          GR3 = ifelse(all(c(maxyear-3, maxyear) %in% Year), (VALUE[Year == maxyear] / VALUE[Year == maxyear-3]) - 1, NA))|>
-  select(GEO, Funder, Performer, `Science type`, Prices, GR5, GR3)|>
+  select(GEO, Funder, Performer, `Science type`, Prices, GR5, GR3, Maxyear)|>
   distinct(.keep_all = TRUE)
   
 
