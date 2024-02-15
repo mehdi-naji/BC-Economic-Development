75aef870716fb0da1e5426a50a5d251870196af7
Naji <Mehdi.Naji@gov.bc.ca>
2024-02-14 17:07:15 -0800

    minor

--- Non_Residential_Investment/Data-processing.R
+++ Non_Residential_Investment/Data-processing.R
@@ -5,22 +5,28 @@ library(lubridate)
 library(stringr)
 
 df1 <- statcan_download_data("36-10-0222-01", "eng")
+df2 <- statcan_download_data("36-10-0108-01", "eng")
 df1$REF_DATE <- ymd(df1$REF_DATE)
 df1$Year <- year(df1$REF_DATE)
+
+df2$REF_DATE <- ymd(df2$REF_DATE)
+df2$Year <- year(df2$REF_DATE)
+
+### TA inja
 
 selected_sectors <- c(11:16, 38, 9)
 filter_conditions  <- sapply(selected_sectors, function(x) grepl(paste0("\\..*\\.", x, "$"), df1$COORDINATE))
 filter_conditions <- apply(filter_conditions, 1, any)
-df2 <- df1[filter_conditions, ]
+df1_2 <- df1[filter_conditions, ]
 
-df2 <- df2 |> 
+df1_2 <- df1_2 |> 
   select(Year, GEO, Prices, Estimates, UOM, SCALAR_FACTOR, VALUE) |>
   filter(Prices %in% c("Current prices", "Chained (2017) dollars")) |>
   group_by(Year, GEO, Prices) |>
   mutate(GDP = first(VALUE[Estimates == "Gross domestic product at market prices"]),
          Estimate_per_GDP = round(100*VALUE/GDP,1))
 
-df3 <- df2 |>
+df1_3 <- df1_2 |>
   group_by(Year, GEO, Prices, UOM, SCALAR_FACTOR ) |>
   filter(
     Estimates %in% c("Non-residential structures, machinery and equipment","Intellectual property products")) |>
@@ -32,8 +38,8 @@ df3 <- df2 |>
     ungroup() |>
   select(Year, GEO, Prices, Estimates, UOM, SCALAR_FACTOR, VALUE, GDP, Estimate_per_GDP)
                
-df2<- merge(df2, df3, all = TRUE)
+df1_2<- merge(df1_2, df1_3, all = TRUE)
             
 # write.csv(df1, "~/StrongerBC-Project/Data/VA_Exporsts_1.csv", row.names = FALSE)
-write.csv(df2, "C:/Users/MNAJI/StrongerBC-Project/Data/Non_Residential_Investment_1.csv", row.names = FALSE)
+write.csv(df1_2, "C:/Users/MNAJI/StrongerBC-Project/Data/Non_Residential_Investment_1.csv", row.names = FALSE)
 
