052133b42c39b03efd25ca192be40dcb3ceffa4e
Naji <Mehdi.Naji@gov.bc.ca>
2024-02-14 16:09:22 -0800

    update nonresidential investment data

--- Non_Residential_Investment/Data-processing.R
+++ Non_Residential_Investment/Data-processing.R
@@ -8,7 +8,7 @@ df1 <- statcan_download_data("36-10-0222-01", "eng")
 df1$REF_DATE <- ymd(df1$REF_DATE)
 df1$Year <- year(df1$REF_DATE)
 
-selected_sectors <- c(11:16, 38)
+selected_sectors <- c(11:16, 38, 9)
 filter_conditions  <- sapply(selected_sectors, function(x) grepl(paste0("\\..*\\.", x, "$"), df1$COORDINATE))
 filter_conditions <- apply(filter_conditions, 1, any)
 df2 <- df1[filter_conditions, ]
@@ -20,6 +20,20 @@ df2 <- df2 |>
   mutate(GDP = first(VALUE[Estimates == "Gross domestic product at market prices"]),
          Estimate_per_GDP = round(100*VALUE/GDP,1))
 
+df3 <- df2 |>
+  group_by(Year, GEO, Prices, UOM, SCALAR_FACTOR ) |>
+  filter(
+    Estimates %in% c("Non-residential structures, machinery and equipment","Intellectual property products")) |>
+  summarise(Estimates = "Non-residential Investment",
+            VALUE = sum(VALUE), 
+            GDP = min(GDP), 
+            Estimate_per_GDP = sum(Estimate_per_GDP)
+            ) |>
+    ungroup() |>
+  select(Year, GEO, Prices, Estimates, UOM, SCALAR_FACTOR, VALUE, GDP, Estimate_per_GDP)
+               
+df2<- merge(df2, df3, all = TRUE)
+            
 # write.csv(df1, "~/StrongerBC-Project/Data/VA_Exporsts_1.csv", row.names = FALSE)
 write.csv(df2, "C:/Users/MNAJI/StrongerBC-Project/Data/Non_Residential_Investment_1.csv", row.names = FALSE)
 
